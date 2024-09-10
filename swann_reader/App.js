4aa2cf99bebf8f228b8ba85aca896c658c37619d
hamza.maal <hamza.maal@gmail.com>
2024-09-10 15:12:39 +0200

    Splash Screen

---
+++ swann_reader/App.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import SplashScreen from './screens/SplashScreen'; // Ensure this matches the actual file name
+
+export default function App() {
+  return <SplashScreen />;
+}
